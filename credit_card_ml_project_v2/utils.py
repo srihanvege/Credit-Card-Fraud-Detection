eed8bf88a633628e7aa92ce70c0ed43b6db5dde6
Srihan Vege <108810677+srihanvege@users.noreply.github.com>
2026-01-19 22:25:27 -0500

    Delete credit_card_ml_project_v2 directory

--- credit_card_ml_project_v2/utils.py
+++
@@ -1,7 +0,0 @@
-from kagglehub import dataset_download
-
-FRAUD_DATASET_ID = "mlg-ulb/creditcardfraud"
-FRAUD_MODEL_PATH = "fraud_xgboost_model.json"
-
-def get_fraud_dataset_path():
-    return dataset_download(FRAUD_DATASET_ID)
